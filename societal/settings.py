4ea9abf3ac97767f123fcbf7a7802d58e11926a9
Bindu Shree Y R <bindushreeyrgnm@gmail.com>
2026-01-17 22:05:12 +0530

    Configure static files for production

--- societal/settings.py
+++ societal/settings.py
@@ -129,6 +129,10 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'societal/static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
