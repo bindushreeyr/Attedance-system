4ec2a36c072d04e8cfc74e4468790a5b1485778e
Bindu Shree Y R <bindushreeyrgnm@gmail.com>
2026-01-17 23:09:24 +0530

    Fix dashboard image filename

--- societal/wsgi.py
+++ societal/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "societal.settings")
 
 application = get_wsgi_application()
+
+
+
+from whitenoise import WhiteNoise
+
+
+
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), "staticfiles"))
