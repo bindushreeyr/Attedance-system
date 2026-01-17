6eb0ebc4558fec74b0d37a76434451493878303e
Bindu Shree Y R <bindushreeyrgnm@gmail.com>
2026-01-17 22:30:05 +0530

    Fix static files configuration

--- societal/settings.py
+++ societal/settings.py
@@ -128,9 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'societal/static')
-]
+
 
 
 
