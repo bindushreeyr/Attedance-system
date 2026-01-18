9abcb172142de2cc76350de1fe79d9c111fcac8f
Bindu Shree Y R <bindushreeyrgnm@gmail.com>
2026-01-18 22:33:17 +0530

    Configured PostgreSQL, whitenoise, reportlab

--- societal/settings.py
+++ societal/settings.py
@@ -83,11 +83,13 @@ WSGI_APPLICATION = "societal.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600,
+        ssl_require=False,
+    )
 }
 
 
